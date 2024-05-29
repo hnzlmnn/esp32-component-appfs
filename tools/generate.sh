a0849db4c1b583d46794b13b7137231573e2d129
Malte Heinzelmann <malte@cybaer.ninja>
2024-05-29 12:26:20 +0200

    Updated for TROOPERS24

--- tools/generate.sh
+++ tools/generate.sh
@@ -3,7 +3,7 @@
 # Usage example
 
 python appfs_generate.py 8192000 appfs.bin
-python appfs_add_file.py appfs.bin ../Apps/python_renze.bin python_tr23 "BadgePython" 1
-python appfs_add_file.py appfs.bin ../../../../tr23_badge_battleship/build/TROOPERSBATTLESHIP.bin battleship "Battleship" 2
-python appfs_add_file.py appfs.bin ../../../../mch2022-esp32-app-gnuboy/build/main.bin gnuboy_tr23 "Gnuboy" 3
+#python appfs_add_file.py appfs.bin ../Apps/python_renze.bin python_tr23 "BadgePython" 1
+python appfs_add_file.py appfs.bin ../../../../troopers24-esp32-app-gnuboy/build/main.bin gnuboy_troopers24 "Gnuboy" 1
+#python appfs_add_file.py appfs.bin ../../../../troopers23-app-ble-keyboard/build/main.bin tr23_ble_keyboard "Bluetooth Keyboard" 4
 #python appfs_add_file.py appfs.bin ../Apps/ESP32\ binaries/gnuboy.bin gnuboy "GNUBOY Game Boy emulator" 1
