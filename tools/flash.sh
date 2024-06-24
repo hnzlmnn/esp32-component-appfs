8fb0519763ba798f19bd914c1a27f7eeef1af745
Malte Heinzelmann <malte@cybaer.ninja>
2024-06-25 01:20:55 +0200

    Adjusted for new partition layout

--- tools/flash.sh
+++ tools/flash.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-esptool.py --port /dev/ttyUSB0 --baud 960000 write_flash 0x330000 appfs.bin
+esptool.py --port /dev/ttyUSB0 --baud 960000 write_flash 0x3d0000 appfs.bin
--- tools/generate.sh
+++ tools/generate.sh
@@ -2,7 +2,7 @@
 
 # Usage example
 
-python appfs_generate.py 8192000 appfs.bin
+python appfs_generate.py 7536640 appfs.bin
 #python appfs_add_file.py appfs.bin ../Apps/python_renze.bin python_tr23 "BadgePython" 1
 python appfs_add_file.py appfs.bin ../../../../troopers24-esp32-app-gnuboy/build/main.bin gnuboy_troopers24 "Gnuboy" 1
 #python appfs_add_file.py appfs.bin ../../../../troopers23-app-ble-keyboard/build/main.bin tr23_ble_keyboard "Bluetooth Keyboard" 4
